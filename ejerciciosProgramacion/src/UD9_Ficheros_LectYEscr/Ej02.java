d325583afe60973fb47ad7fa42082bfdf1970170
luis(casa) <lperezhurtado97@gmail.com>
2022-03-22 13:54:08 +0100

    Lectura y escritura ficheros

--- ejerciciosProgramacion/src/UD9_Ficheros_LectYEscr/Ej02.java
+++ ejerciciosProgramacion/src/UD9_Ficheros_LectYEscr/Ej02.java
@@ -14,21 +14,24 @@ public class Ej02 {
             File f = new File("Documentos/alumnos_notas.txt");
             Scanner lector = new Scanner(f);
             ArrayList<String> lista = new ArrayList();
+            ArrayList<Integer> notas = new ArrayList();
             String[] num;
+            double media;
             int cont = 0;
+            
             while(lector.hasNext()){
                 lista.add(lector.nextLine());
-                System.out.println(lista.get(cont));
                 num = lista.get(cont).split(" ");
                 
+                media = 0;
+                for (int i = 2; i < num.length; i++) {
+                    media += Integer.parseInt(num[i]);  
+                }
+                
+                System.out.println(num[0]+" "+num[1]+": "+media/(num.length-2));
                 cont++;
-                
-                
             }
-            
-            
-        }
-        
+        } 
         catch(FileNotFoundException e){
             System.out.println(e);
         }
